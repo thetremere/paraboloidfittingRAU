6bdda6b5d55bdada4db684dca35159b90bd99f28
thetremere <65847588+thetremere@users.noreply.github.com>
2020-06-04 09:12:31 +0400

    Update fittingLine.py

--- fittingLine.py
+++ fittingLine.py
@@ -3,26 +3,19 @@ import numpy as np
 pts = np.add.accumulate(np.random.random((10,3)))
 x,y,z = pts.T
 
-# this will find the slope and x-intercept of a plane
-# parallel to the y-axis that best fits the data
 A_xz = np.vstack((x, np.ones(len(x)))).T
 m_xz, c_xz = np.linalg.lstsq(A_xz, z)[0]
 
-# again for a plane parallel to the x-axis
 A_yz = np.vstack((y, np.ones(len(y)))).T
 m_yz, c_yz = np.linalg.lstsq(A_yz, z)[0]
 
-# the intersection of those two planes and
-# the function for the line would be:
-# z = m_yz * y + c_yz
-# z = m_xz * x + c_xz
-# or:
+
 def lin(z):
     x = (z - c_xz)/m_xz
     y = (z - c_yz)/m_yz
     return x,y
 
-#verifying:
+
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 
